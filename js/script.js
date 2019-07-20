c2b9f84253aaffa9579b7fa79e499a24b54875b2
Valeria Nazarova <alira.vn@gmail.com>
2019-07-20 02:18:14 +0200

    extend select options and js function for errMessages

--- js/script.js
+++ js/script.js
@@ -3,20 +3,25 @@ function calculateInsurance() {
 	var age = Number(document.getElementById("age").value); // gets value of age from form
 	var country = document.getElementById("country").value; // gets value of country from form
 	var hps = Number(document.getElementById("hps").value); // gets value of hps from form
-	// console.log(name, age, country, hps);
+	var errMessageNaN = "</b>, we cannot proceed with your request. <br>Please make sure, that you input correct data in each field.";
+	var errMessageContact = "</b>, at at the moment we cannot provide calculation for your request. Please, contact us directly to get a quote <br>(Email <b>request@cf-insurance.at</b>)";
+	console.log(name, age, country, hps);
 	// add checker for numbers and restart the calculation again
 	if (isNaN(age) || isNaN(hps)) {
-		document.getElementById("output").innerHTML = "Dear <b>" + name + "</b>, we cannot proceed with your request. Please make sure, that you input correct data in each field."
+		document.getElementById("output").innerHTML = "Dear <b>" + name + errMessageNaN;
 	} else if (country == "at") {
 		var result = hps * 100 / age + 50; // testcase Austria - OK
 	} else if (country == "hu") {
 		var result = hps * 120 / age + 100; // testcase Hungary - OK
-	} else {
+	} else if (country == "gr") {
 		var result = ((hps * 150) / (age + 3)) + 50; // testcase Greece - Ok
+	} else {
+		//document.getElementById("output").style.fontSize = "1.8vw"; // changes font-size of div to fit in a long message, how to return it back afterwrds?
+		document.getElementById("output").innerHTML = "Dear <b>" + name + errMessageContact;
 	}
 	// console.log(result);
 	// create 4th testcase with different output
 	var insurance = result.toFixed(2); // rounds value of var result to max. 1 dec. point
-	document.getElementById("output").innerHTML = "Dear <b>" + name + "</b>, insurance for your car will cost <b>" + insurance + " EUR </b>";
+	document.getElementById("output").innerHTML = "Dear <b>" + name + "</b>, insurance for your car will cost <b>" + insurance + " EUR.</b>";
 }
 	document.getElementById("calc-button").addEventListener("click", calculateInsurance, false);
